ca2a960dd9941a75868a04d58e0f6e6d0eb28868
tcapris <61629953+tcapris@users.noreply.github.com>
2021-02-22 17:21:01 +0000

    Função Incrementar botão

--- Aulas NWL/moveit/src/components/Button.tsx
+++ Aulas NWL/moveit/src/components/Button.tsx
@@ -1,13 +1,27 @@
+import {useState} from 'react';
+
 interface ButtonProps {
-    color: string,
-    children: string
+    color: string;
+    children: string;
 }
 
 export function Button(props: ButtonProps) {
+    const [counter, setCounter] = useState(1)
+
+    function incrementCounter(){
+        setCounter(counter + 1)
+
+    }
 
     return (
-        <button type="button" style={{ backgroundColor: props.color }} >
+        <button 
+        type="button" 
+        style={{ backgroundColor: props.color }} 
+        onClick={incrementCounter}
+        >
             {props.children}
+
+            <strong>{counter}</strong>
 
 
         </button>
