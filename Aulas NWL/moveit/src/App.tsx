9b599ee6e8a5e7594fbc79baa32140d185229a07
tcapris <61629953+tcapris@users.noreply.github.com>
2021-02-22 16:59:39 +0000

    children example

--- Aulas NWL/moveit/src/App.tsx
+++ Aulas NWL/moveit/src/App.tsx
@@ -4,9 +4,19 @@ import { Button } from '../src/components/Button'
 function App() {
   return (
     <div>
-      <Button color="red" />
-      <Button color="blue" />
-      <Button color="purple" />
+      <Button color="red">
+        Botão 1
+
+      </Button>
+      <Button color="blue">
+
+      Botão 2
+
+      </Button>
+      <Button color="purple">
+      Botão 3
+
+      </Button>
     </div>
 
   );
